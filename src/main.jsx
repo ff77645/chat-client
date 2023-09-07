066ae4cdb78bbaa04734dfcbf22346cb608c822f
isFY <fy77645@126.com>
2023-09-07 21:48:27 +0800

    添加nextui

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {NextUIProvider} from '@nextui-org/react'
 import {BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
@@ -7,7 +8,11 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NextUIProvider>
+        <div className="dark">
+          <App />
+        </div>
+      </NextUIProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
